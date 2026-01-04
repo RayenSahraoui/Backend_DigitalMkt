8c16b04c01945d5072701b0f5a70d03ef0fcf21b
Rayen Sahraoui <rayen.sahraoui@esprit.tn>
2026-01-04 21:06:25 +0100

    Add Strapi APIs and types

---
+++ src/api/click-number/controllers/click-number.ts
@@ -0,0 +1,7 @@
+/**
+ * click-number controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::click-number.click-number');
---
+++ src/api/click-number/services/click-number.ts
@@ -0,0 +1,7 @@
+/**
+ * click-number service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::click-number.click-number');
